3153925f8c064ea804e853c998392dc3094744f4
Pabitra <pabitram322@gmail.com>
2025-09-05 14:22:43 +0000

    create a horizontalScroll animation

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -3,13 +3,14 @@ import { motion } from "framer-motion";
 
 export function AnimatedBackground() {
   return (
-    <video
-      className="fixed top-0 left-0 w-screen h-screen object-cover z-0"
-      src="/bg-video2.mp4"
-      autoPlay
-      loop
-      muted
-      playsInline
-    />
+    // <video
+    //   className="fixed top-0 left-0 w-screen h-screen object-cover z-0"
+    //   src="/bg-video2.mp4"
+    //   autoPlay
+    //   loop
+    //   muted
+    //   playsInline
+    // />
+    <div></div>
   );
 }
